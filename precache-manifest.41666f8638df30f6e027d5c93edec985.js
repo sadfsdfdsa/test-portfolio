4b0e97f45d4c26eb734e6bfebf2dd8cb2bc8e7f7
Artem Shuvaev <shuvaev13@mail.ru>
2021-04-25 15:01:53 +0500

    deploy

---
+++ precache-manifest.41666f8638df30f6e027d5c93edec985.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527a97a4e012a0f261d6",
+    "url": "/test-portfolio/css/app.e054459a.css"
+  },
+  {
+    "revision": "758785db432ee77c21a3",
+    "url": "/test-portfolio/css/chunk-2df754bc.28efcc09.css"
+  },
+  {
+    "revision": "61a10f3faedc7643b355",
+    "url": "/test-portfolio/css/chunk-vendors.410732e9.css"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/test-portfolio/fonts/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/test-portfolio/fonts/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/test-portfolio/fonts/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/test-portfolio/fonts/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/test-portfolio/fonts/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/test-portfolio/fonts/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "6150bc7e29cc2295d50fdc7e1dc5614b",
+    "url": "/test-portfolio/index.html"
+  },
+  {
+    "revision": "527a97a4e012a0f261d6",
+    "url": "/test-portfolio/js/app.4da4c072.js"
+  },
+  {
+    "revision": "758785db432ee77c21a3",
+    "url": "/test-portfolio/js/chunk-2df754bc.3ea44dd7.js"
+  },
+  {
+    "revision": "61a10f3faedc7643b355",
+    "url": "/test-portfolio/js/chunk-vendors.a444bbd8.js"
+  },
+  {
+    "revision": "08f199186ff87f287606684c5e58ffe7",
+    "url": "/test-portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test-portfolio/robots.txt"
+  }
+]);
